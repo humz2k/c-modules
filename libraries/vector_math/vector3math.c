acdef284b2af30c1cfa9a1c525a3325691e392db
Humza Qureshi <humzaqureshis@gmail.com>
2024-02-27 14:36:10 -0600

    cmod

--- libraries/vector_math/vector3math.c
+++ libraries/vector_math/vector3math.c
@@ -1,5 +1,6 @@
 import cmath;
 import vector3struct;
+import vector3arith;
 
 float v3len2(vec3 l){
     return l.x*l.x + l.y*l.y + l.z*l.z;
@@ -7,4 +8,12 @@ float v3len2(vec3 l){
 
 float v3len(vec3 l){
     return sqrtf(v3len2(l));
+}
+
+float v3dist(vec3 l, vec3 r){
+    return v3len(v3sub(l,r));
+}
+
+float v3dist2(vec3 l, vec3 r){
+    return v3len2(v3sub(l,r));
 }
--- test.c
+++ test.c
@@ -1,3 +1,8 @@
+/*
+    - Run this file with cmod test.c
+    - This will run the file and build test.o.
+*/
+
 import std;
 import io;
 import hello_world_library;
